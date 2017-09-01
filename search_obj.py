e05beb7b9caf7d1e06214659a438556aa2af01d7
Ucnt <matt.svensson@gmail.com>
2017-09-01 13:33:22 -0400

    Update search_obj.py

--- search_obj.py
+++ search_obj.py
@@ -14,11 +14,11 @@ class SearchNames():
 
 
 class SearchStrings():
-    def __init__(self, num_chars, num_chars_range, string_options, threads, print_bucket_names, output_file, start_after):
+    def __init__(self, num_chars, num_chars_range, string_options, threads, print_bucket_names, output_file, start_after_value):
         self.num_chars = num_chars
         self.num_chars_range = num_chars_range
         self.string_options = string_options
         self.threads = threads
         self.print_bucket_names = print_bucket_names
         self.output_file = output_file
-        self.start_after = start_after
+        self.start_after_value = start_after_value
